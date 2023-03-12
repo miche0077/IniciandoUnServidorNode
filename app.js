c82e2fb7040a737d5dc22bb9b1bba405631fcc11
Michelle Vegas <88907873+miche0077@users.noreply.github.com>
2023-03-12 17:16:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express()
+app.use(express.json());
+
+
 
 app.get('/', (req, res)=>{
     res.send('<h1>My First Server!</h1>')
 })
+
+//exercicio 4  
+app.get('/envia/:name', (request, response) => {
+    let name = request.params.name;
+    response.send(`Rota de API criada pelo: ${name}`);
+    console.log(`Rota de API criada pelo: ${name}`);
+});
+
+//exercicio 5
+app.post('/enviaObjeto', (request, response) => {
+    const object = request.body;
+    response.json();
+    console.log(`Enviado com sucesso ${JSON.stringify(object)}`);
+});
+
 app.listen(3000, ()=>{
     console.log('Init Server!')
 })
-
